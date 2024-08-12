8254c52dea3d0d841f3339255e1071eda4449487
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-08-12 23:01:37 +0300

    add css reset

---
+++ index.js
@@ -0,0 +1,12 @@
+// create header with icon
+// create main with button add book
+// create form with title, author, number of pages, is read
+// create footer
+
+// three colors 1. 0c1618 2. 004643 3.faf4d3
+
+const myLibrary = [];
+
+function Book() {}
+
+function addBookToLibrary() {}
