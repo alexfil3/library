631e7a0f7eb00af270915efd914470dd0ce42dbe
Alex Filimonov <alex.filimonov2002@gmail.com>
2024-08-13 10:50:55 +0300

    create a from and add some functionality

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-// create header with icon
-// create main with button add book
-// create form with title, author, number of pages, is read
-// create footer
-
 // three colors 1. 0c1618 2. 004643 3.faf4d3
+
+const addBook = document.querySelector(".add-book");
+const dialog = document.querySelector("dialog");
+const submit = dialog.querySelector(".submit");
 
 const myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addBookToLibrary() {}
+
+addBook.addEventListener("click", openModal);
+
+function openModal() {
+  dialog.showModal();
+}
+
+submit.addEventListener("click", addBookToLibrary);
